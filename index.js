79e234fe5e4ecc0f270f7a5ba2a6566dad36b1de
JedwardMook <jedwardmook@yahoo.com>
2022-02-10 18:49:03 -0600

    Images to json and fetch in js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 console.log("hello")
 
+function getAlbums() {
+    fetch('http://localhost:3000/albums')
+    .then(response => response.json())
+    .then(albumData => console.log(albumData))
+    };
+getAlbums()
+
 
 
 
