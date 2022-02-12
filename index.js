9431ccfca2824c9ab1f70df18cb763e127e76f04
JedwardMook <jedwardmook@yahoo.com>
2022-02-12 13:53:02 -0600

    Back and forth changes
    Tried to use a html template to make album divs to help with the search bar, ultimately had to go back to using function to render albums on page using forEach
    Made changes to CSS specifically cards appearance and gave the body a background color as well

--- index.js
+++ index.js
@@ -4,9 +4,35 @@ console.log("hello")
 function getAlbums() {
     fetch('http://localhost:3000/albums')
     .then(response => response.json())
-    .then(albumData => console.log(albumData))
+    .then(albumData => albumData.forEach(renderAlbum))
     };
 getAlbums()
+
+function renderAlbum(album){
+    let albumCard = document.createElement('div')
+    albumCard.className ="card"
+    albumCard.id ="card"
+    albumCard.innerHTML = `
+    <div class= "album-image">
+        <img src=${album.image} alt="Album Cover" width="200px" height="200px">
+    </div>
+    <div class= "album-info">
+        <h3>${album.name}</h3>
+        <h4>Artist: ${album.artist}</h4>
+        <p>Genre: ${album.genre}</p>
+        <p>Released: ${album.release}</p>
+    </div>
+    <div class="remove">
+        <button id="remove">Remove Album</button>
+    </div>`
+
+    const albumContainer = document.querySelector('#album-container')
+    albumContainer.append(albumCard)
+    albumCard.querySelector('#remove').addEventListener('click', () => {
+        albumCard.remove()
+        deleteAlbum(album.id)
+    })
+};
 
 
 
