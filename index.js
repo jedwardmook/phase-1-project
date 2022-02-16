926eb37b38cb918da3f34d674551ceb0d050b8ac
JedwardMook <jedwardmook@yahoo.com>
2022-02-16 14:18:58 -0600

    places search bar in collection, removes search toggle

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ let addAlbum = false;
 document.addEventListener("DOMContentLoaded", () => {
 
 let gotAlbums = []
+
 const searchBar = document.querySelector(".search");
 const albumContainer = document.querySelector('#album-container')
-
+//push
 const viewCollection = () => {
     const viewBtn = document.getElementById("view-btn");
     viewBtn.addEventListener("click", () =>{
@@ -25,30 +26,16 @@ const toggleView = () => {
   viewAlbum = !viewAlbum;
     if (viewAlbum) {
       albumContainer.style.display = "grid",
+      searchBar.style.display= "grid",
       viewBtn.innerText = "Hide Collection";
     } else {
       albumContainer.style.display = "none",
+      searchBar.style.display= "none",
       viewBtn.innerText = "View Collection";
     }
  })
 };
 toggleView()
-
-const toggleSearches = () => {
-    const togSearchBtn = document.getElementById("search-btn");
-    togSearchBtn.addEventListener("click", () => {
-    
-      searchAlbum = !searchAlbum;
-      if (searchAlbum) {
-        searchBar.style.display = "grid",
-        togSearchBtn.innerText = "Hide Search";
-      } else {
-        searchBar.style.display = "none",
-        togSearchBtn.innerText = "Search Collection";
-      }
-   })
-};
-toggleSearches()
 
 const toggleAdd = () => {
   const togAddBtn = document.getElementById("add-btn");
@@ -76,7 +63,7 @@ function getAlbums() {
       gotAlbums = albumData})
     };
 getAlbums()
-
+//read callback again
 function removeAlbum(id) {
   fetch(`http://localhost:3000/albums/${id}`,{
       method: "DELETE",
@@ -98,10 +85,11 @@ function logAlbum(newAlbumObj){
     body: JSON.stringify(newAlbumObj)
     })
     .then(response => response.json())
-    .then(response => console.log(newAlbumObj))
+    .then(response => gotAlbums.push(newAlbumObj))
+    gotAlbums.forEach(renderAlbum)  
 };
 //fetches
-
+//es6 object destructuring, object property value shorthand
 const newAlbumForm = () => {
   const albumForm = document.querySelector(".add-album-form")
   albumForm.addEventListener("submit", (e) => {
@@ -136,27 +124,28 @@ function searchCollection() {
   })
 };
 searchCollection()
-
-
+//put added albums directly in array
+//code challenge- define functions/ if else/ array methods/ dom manipulations, give a play by play
 
 function renderAlbum(album){
-  let albumCard = document.createElement('div')
+  const {image, name, artist, genre, release} = album
+  const albumCard = document.createElement('div')
   albumCard.className ="card"
   albumCard.id ="card"
   albumCard.innerHTML = `
   <div class= "album-image">
-      <img src=${album.image} alt="Album Cover" width="200px" height="200px">
+      <img src=${image} alt="Album Cover" width="200px" height="200px">
   </div>
   <div class= "album-info">
-      <h3>${album.name}</h3>
-      <h4>Artist: ${album.artist}</h4>
-      <p>Genre: ${album.genre}</p>
-      <p>Released: ${album.release}</p>
+      <h3>${name}</h3>
+      <h4>Artist: ${artist}</h4>
+      <p>Genre: ${genre}</p>
+      <p>Released: ${release}</p>
   </div>
   <div class="remove">
       <button id="remove">Remove Album</button>
   </div>`
-
+// unstanding variables, functions, callbacks, array methods, scope
   albumContainer.append(albumCard)
   albumCard.querySelector('#remove').addEventListener('click', () => {
       albumCard.remove()
