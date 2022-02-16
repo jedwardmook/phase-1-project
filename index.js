caa71c036f2e2904c226a57003777cbe0a978c6d
JedwardMook <jedwardmook@yahoo.com>
2022-02-15 21:51:53 -0600

    a bunch of back and forth figuring out search

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
 let gotAlbums = []
 const searchBar = document.querySelector(".search");
 
+
 const viewCollection = () => {
     const viewBtn = document.getElementById("view-btn");
     viewBtn.addEventListener("click", () =>{
@@ -121,39 +122,44 @@ const newAlbumForm = () => {
 }
 newAlbumForm()
 
-searchBar.addEventListener("input", (e) => {
-  const searchString = e.target.value.toLowerCase();
-  const filteredAlbums = gotAlbums.filter( album => {
-    return album.artist.toLowerCase().includes(searchString)
+function searchCollection() {
+  searchBar.addEventListener("input", (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredAlbums = gotAlbums.filter( album => {
+    return album.artist.toLowerCase().includes(searchString) ||
+            album.name.toLowerCase().includes(searchString)
+    })
+    console.log(filteredAlbums)
   })
-  console.log(filteredAlbums)
+};
+searchCollection()
 
-})
+
 
 function renderAlbum(album){
-    let albumCard = document.createElement('div')
-    albumCard.className ="card"
-    albumCard.id ="card"
-    albumCard.innerHTML = `
-    <div class= "album-image">
-        <img src=${album.image} alt="Album Cover" width="200px" height="200px">
-    </div>
-    <div class= "album-info">
-        <h3>${album.name}</h3>
-        <h4>Artist: ${album.artist}</h4>
-        <p>Genre: ${album.genre}</p>
-        <p>Released: ${album.release}</p>
-    </div>
-    <div class="remove">
-        <button id="remove">Remove Album</button>
-    </div>`
+  let albumCard = document.createElement('div')
+  albumCard.className ="card"
+  albumCard.id ="card"
+  albumCard.innerHTML = `
+  <div class= "album-image">
+      <img src=${album.image} alt="Album Cover" width="200px" height="200px">
+  </div>
+  <div class= "album-info">
+      <h3>${album.name}</h3>
+      <h4>Artist: ${album.artist}</h4>
+      <p>Genre: ${album.genre}</p>
+      <p>Released: ${album.release}</p>
+  </div>
+  <div class="remove">
+      <button id="remove">Remove Album</button>
+  </div>`
 
-    const albumContainer = document.querySelector('#album-container')
-    albumContainer.append(albumCard)
-    albumCard.querySelector('#remove').addEventListener('click', () => {
-        albumCard.remove()
-        removeAlbum(album.id)
-    })
+  const albumContainer = document.querySelector('#album-container')
+  albumContainer.append(albumCard)
+  albumCard.querySelector('#remove').addEventListener('click', () => {
+      albumCard.remove()
+      removeAlbum(album.id)
+  })
 };
 
 
@@ -162,11 +168,3 @@ function renderAlbum(album){
 
 
 });
-
-
-
-
-
-
-
-
